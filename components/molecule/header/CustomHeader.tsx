e0a6e93538c41589508a1118bf067afab21fb833
LEE BO HYUN <92661884+frombozztoang@users.noreply.github.com>
2023-08-15 18:25:57 +0900

    feat : custom header

--- components/molecule/header/CustomHeader.tsx
+++ components/molecule/header/CustomHeader.tsx
@@ -1,8 +1,14 @@
 import React from "react";
-import { View, Pressable } from "react-native";
+import {
+  View,
+  Pressable,
+  Platform,
+  SafeAreaView as iOSSafeAreaView,
+} from "react-native";
 import BackButton from "../../../assets/svg/BackButton";
 import styled from "styled-components/native";
 import MolText from "../../atom/Text/Text";
+import { SafeAreaView as AndroidSafeAreaView } from "react-native-safe-area-context";
 
 interface CustomHeaderProps {
   navigation: any;
@@ -17,28 +23,42 @@ const CustomHeader = ({
   showBackButton = false,
   showBottomLine = false,
 }: CustomHeaderProps) => {
+  const SafeAreaViewComponent =
+    Platform.OS === "ios" ? iOSSafeAreaView : AndroidSafeAreaView;
+
   return (
-    <HeaderContainer>
-      {showBackButton && (
-        <Pressable onPress={() => navigation.goBack()}>
-          <BackButton />
-        </Pressable>
-      )}
-      <MolText label={title} size="17" weight="regular" align="center" />
-      {showBottomLine && <BottomLine />}
-    </HeaderContainer>
+    <SafeAreaViewComponent>
+      <HeaderContainer>
+        {showBackButton && (
+          <Pressable onPress={() => navigation.goBack()}>
+            <BackButtonContainer>
+              <BackButton />
+            </BackButtonContainer>
+          </Pressable>
+        )}
+        <MolText label={title} size="17" weight="regular" align="center" />
+
+        {showBottomLine && <BottomLine />}
+      </HeaderContainer>
+    </SafeAreaViewComponent>
   );
 };
 
 const HeaderContainer = styled.View`
-  flex-direction: row;
-  align-items: center;
+  padding-top: 8px;
 `;
 
 const BottomLine = styled.View`
   width: 390px;
+  margin-top: 13px;
   height: 0.4px;
+
   background: #333;
+`;
+
+const BackButtonContainer = styled.View`
+  flex: 1;
+  background-color: rebeccapurple;
 `;
 
 export default CustomHeader;
