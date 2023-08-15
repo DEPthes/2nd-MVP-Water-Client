875a0b84ba1fe9625fb407ae40715314ccc8d255
_yoonyoon_1 <heeyoon1302@naver.com>
2023-08-15 14:21:04 +0900

    feat : 홈 화면 배경 설정

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -2,16 +2,28 @@ import { View, Text, Pressable } from "react-native";
 import React from "react";
 import MolText from "../components/atom/Text/Text";
 import styled from "styled-components/native";
+import { LinearGradient } from "expo-linear-gradient";
 
 const Home = ({ navigation }: { navigation: any }) => {
   return (
     <Pressable onPress={() => navigation.navigate("DiaryWriting")}>
       <Container>
-        <MolText
-          label="화면을 터치하면 일기를 작성할 수 있습니다"
-          size="14"
-          weight="regular"
-        />
+        <LinearGradient
+          colors={["rgba(0, 148, 255, 0.53)", "rgba(209, 236, 255, 0)"]}
+          style={{
+            flex: 1,
+            width: "100%",
+            height: "100%",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <MolText
+            label="화면을 터치하면 일기를 작성할 수 있습니다"
+            size="14"
+            weight="regular"
+          />
+        </LinearGradient>
       </Container>
     </Pressable>
   );
